8b8ca2e6a8183c5c3965b3dbc30bd91ce90aeee1
Fakhry Mubarak <fakhrymubarak@gmail.com>
2021-01-24 20:07:47 +0800

    refactor : make code more efficient and add some comment documentation

--- app/src/main/java/com/fakhry/lifelog/base/BaseFunction.kt
+++ app/src/main/java/com/fakhry/lifelog/base/BaseFunction.kt
@@ -7,20 +7,45 @@ import java.util.*
 
 class BaseFunction(context: Context) {
 
-    fun epochToDate(dateInMilliseconds: String): String {
-        val dateFormat = "dd/MM/yyyy"
-        return DateFormat.format("dd/MM/yyyy", dateInMilliseconds.toLong()).toString()
-    }
+    /**
+     * @param timeMillis format should : epoch format(example : 1611475065784)
+     * @return will be : "dd/MM/yyyy,HH:mm"
+     * */
+    private fun epochToDate(timeMillis: Long): String =
+        DateFormat.format("dd/MM/yyyy,HH:mm", timeMillis).toString()
 
-    fun dateToFormalString(completeDate: String): String {
-        val format = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault()).parse(completeDate)!!
-        //Sun Jan 31 00:00:00
+    /**
+     * this function is wrapper from 'epochToDate' -> 'dateFormatString'
+     * @param timeMillis format should : epoch format(example : 1611475065784)
+     * @return format will be : "Sunday, 24 January 2021 or Sunday, 24 January 2021,16:50"
+     * */
+    fun getFormalDate(timeMillis: Long = System.currentTimeMillis(), withHours: Boolean): String =
+        dateToFormalString(epochToDate(timeMillis), withHours)
 
-        val finalDay = SimpleDateFormat("EEEE", Locale.getDefault()).format(format)
-        val finalDate = SimpleDateFormat("dd", Locale.getDefault()).format(format)
-        val finalMonth = SimpleDateFormat("MMMM", Locale.getDefault()).format(format)
-        val finalYear = SimpleDateFormat("yyyy", Locale.getDefault()).format(format)
+    /**
+     * @param completeDate format should : dd/MM/yyyy,HH:mm
+     * @return will be : "Sunday, 24 January 2021 || Sunday, 24 January 2021,16:49"
+     * */
+    fun dateToFormalString(completeDate: String, withHours: Boolean): String {
+        lateinit var formalStringDate: String
+        val format = SimpleDateFormat("dd/MM/yyyy,HH:mm", Locale.getDefault()).parse(completeDate)
+        formalStringDate = if (format != null) {
+            val finalDay = SimpleDateFormat("EEEE", Locale.getDefault()).format(format)
+            val finalDate = SimpleDateFormat("dd", Locale.getDefault()).format(format)
+            val finalMonth = SimpleDateFormat("MMMM", Locale.getDefault()).format(format)
+            val finalYear = SimpleDateFormat("yyyy", Locale.getDefault()).format(format)
+            val finalHour = SimpleDateFormat("HH", Locale.getDefault()).format(format)
+            val finalMinute = SimpleDateFormat("mm", Locale.getDefault()).format(format)
+            if (withHours) {
+                "$finalDay, $finalDate $finalMonth $finalYear, $finalHour:$finalMinute"
 
-        return "$finalDay, $finalDate $finalMonth $finalYear"
+            } else {
+                "$finalDay, $finalDate $finalMonth $finalYear"
+
+            }
+        } else {
+            ""
+        }
+        return formalStringDate
     }
 }
