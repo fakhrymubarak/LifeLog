df1be6cb8ad63f93c1878939a1e70a1692c6c50c
Fakhry Mubarak <fakhrymubarak@gmail.com>
2021-01-29 17:15:25 +0800

    feat : finish get note based calendar

--- app/src/main/java/com/fakhry/lifelog/ui/fragments/calendar/CalendarFragment.kt
+++ app/src/main/java/com/fakhry/lifelog/ui/fragments/calendar/CalendarFragment.kt
@@ -5,8 +5,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.fakhry.lifelog.base.BaseFunction
+import com.fakhry.lifelog.data.local.entities.NoteEntity
 import com.fakhry.lifelog.databinding.FragmentCalendarBinding
+import com.fakhry.lifelog.ui.adapters.ListNoteAdapter
+import com.fakhry.lifelog.viewmodel.ViewModelFactory
 
 class CalendarFragment : Fragment() {
 
@@ -26,16 +31,49 @@ class CalendarFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         baseFunction = BaseFunction(requireContext())
-        binding.tvSelectedDate.text = baseFunction.getFormalDate(withHours = false)
+
+        val dateRightNow = baseFunction.getFormalDate(withHours = false)
+        binding.tvSelectedDate.text = dateRightNow
+
+        val factory = ViewModelFactory.getInstance(requireContext())
+        calendarViewModel = ViewModelProvider(this, factory)[CalendarViewModel::class.java]
+        setViewModel(dateRightNow)
 
         binding.calendar.setOnDateChangeListener { _, year, month, date ->
-            updateNoteList(date, month + 1, year)
+            populateView(date, month + 1, year)
         }
     }
 
-    private fun updateNoteList(date: Int, month: Int, year: Int) {
+    private fun populateView(date: Int, month: Int, year: Int) {
         val completeDate = "$date/${month}/$year,00:00"
-        binding.tvSelectedDate.text =
-            baseFunction.dateToFormalString(completeDate, withHours = false)
+        val dateSelected = baseFunction.dateToFormalString(completeDate, false)
+        binding.tvSelectedDate.text = dateSelected
+        setViewModel(dateSelected)
+    }
+
+    private fun setViewModel(dateSelected: String) {
+        calendarViewModel.getNoteBasedDate(dateSelected).observe(viewLifecycleOwner, { listNote ->
+            if (!listNote.isNullOrEmpty()) {
+                binding.ivIllustNothing.visibility = View.GONE
+                binding.tvNothing.visibility = View.GONE
+                binding.rvNoteCalendar.visibility = View.VISIBLE
+                setRecyclerViewNote(listNote)
+            } else {
+                binding.ivIllustNothing.visibility = View.VISIBLE
+                binding.tvNothing.visibility = View.VISIBLE
+                binding.rvNoteCalendar.visibility = View.GONE
+            }
+        })
+    }
+
+    private fun setRecyclerViewNote(listNote: List<NoteEntity>) {
+        binding.rvNoteCalendar.setHasFixedSize(true)
+        val childNoteAdapter = ListNoteAdapter()
+        childNoteAdapter.notifyDataSetChanged()
+        childNoteAdapter.setData(listNote)
+
+        binding.rvNoteCalendar.layoutManager =
+            LinearLayoutManager(binding.rvNoteCalendar.context, LinearLayoutManager.VERTICAL, false)
+        binding.rvNoteCalendar.adapter = childNoteAdapter
     }
 }
--- app/src/main/java/com/fakhry/lifelog/ui/fragments/calendar/CalendarViewModel.kt
+++ app/src/main/java/com/fakhry/lifelog/ui/fragments/calendar/CalendarViewModel.kt
@@ -3,12 +3,18 @@ package com.fakhry.lifelog.ui.fragments.calendar
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.fakhry.lifelog.data.Repository
+import com.fakhry.lifelog.data.local.entities.NoteEntity
+import kotlinx.coroutines.launch
 
-class CalendarViewModel(mRepository: Repository) : ViewModel() {
+class CalendarViewModel(private val mRepository: Repository) : ViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is calendar Fragment"
+    fun getNoteBasedDate(dateCreated: String): LiveData<List<NoteEntity>> {
+        val listDates = MutableLiveData<List<NoteEntity>>()
+        viewModelScope.launch {
+            listDates.postValue(mRepository.getNotesBasedDate(dateCreated))
+        }
+        return listDates
     }
-    val text: LiveData<String> = _text
 }
