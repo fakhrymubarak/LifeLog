d3ef431508571f7d142060397ceaed19715f1de2
Fakhry <fakhrymubarak@gmail.com>
2025-06-29 17:23:22 +0700

    init new :core:utils module

--- core-old/build.gradle.kts
+++ core-old/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 }
 
 android {
-     namespace = BuildConfig.generateNamespace("core-old")
+     namespace = BuildConfig.generateNamespace("core_old")
 }
 
 dependencies {
---
+++ core/utils/build.gradle.kts
@@ -0,0 +1,17 @@
+plugins {
+    alias(libs.plugins.android.library)
+}
+
+android {
+    namespace = BuildConfig.generateNamespace("core.utils")
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
