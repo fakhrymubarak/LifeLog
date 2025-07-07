676237e127fcd07f2b5a97873f444d4335e5757d
Fakhry <fakhrymubarak@gmail.com>
2025-07-07 22:05:48 +0700

    add: create todo

---
+++ app/src/main/java/com/fakhry/lifelog/App.kt
@@ -0,0 +1,11 @@
+package com.fakhry.lifelog
+
+import android.app.Application
+
+/**
+ * TODO
+ *  - Migrate UI to jetpack compose
+ *  - Add realtime syncing on like ap*le notes
+ *
+ * */
+class LifeLogApp: Application()
