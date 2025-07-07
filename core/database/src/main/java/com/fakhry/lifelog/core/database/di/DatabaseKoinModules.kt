6355540ee0cfb7880e80b4c34989fd00376888cd
Fakhry <fakhrymubarak@gmail.com>
2025-07-07 09:36:30 +0700

    Remove legacy local data classes and dependencies
    This commit removes the following deprecated classes and their associated Koin dependencies:
    
    - `LocalDataRepository.kt`
    - `LocalDataRepositoryImpl.kt`
    - `LocalDataSource.kt`
    
    These classes were part of an older local data handling approach and have been superseded by a new implementation.

--- core/database/src/main/java/com/fakhry/lifelog/core/database/di/DatabaseKoinModules.kt
+++ core/database/src/main/java/com/fakhry/lifelog/core/database/di/DatabaseKoinModules.kt
@@ -2,12 +2,9 @@ package com.fakhry.lifelog.core.database.di
 
 import com.fakhry.lifelog.core.database.room.LifeLogDao
 import com.fakhry.lifelog.core.database.room.LifeLogDatabase
-import com.fakhry.lifelog.core.database.room.LocalDataSource
-import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
 
 fun lifeLogLocalDataSourceModule() = module {
     single<LifeLogDatabase> { LifeLogDatabase.getInstance(get()) }
     single<LifeLogDao> { get<LifeLogDatabase>().lifeLogDao() }
-    singleOf(::LocalDataSource)
 }
